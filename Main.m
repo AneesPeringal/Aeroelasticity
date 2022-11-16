75f466a6617a00360c445375f6ae0b1e033e0515
AneesPeringal <63726975+AneesPeringal@users.noreply.github.com>
2022-11-16 13:03:14 +0400

    routh stability analysis matrix B

--- Main.m
+++ Main.m
@@ -9,10 +9,13 @@ nu = 0.08;
 Omega = 0.5;
 zeta_alpha = 0.01; 
 zeta_h = 0.01;
+lambda =1;
+syms zeta gamma U_tilde
 
 
 
-epsilon = m/M;
+% epsilon = m/M;
+epsilon = 0.05; % obtained from the paper
 % x_tilde = x / b;
 % y = h/b; 
 % x_alpha = S_alpha / (M * b);  % x_cg / b
@@ -23,10 +26,10 @@ M = [1         x_alpha       0 ;
     0            0           1 ];
 
 C = [ zeta_h + epsilon*zeta+ beta* U_tilde , -epsilon*zeta*lambda                  , -epsilon*zeta         ;
-    -nu*U_tilde - eta*zeta*lambda          ,  zeta_alpha+epsilon*zeta*lambda^2     , epsilon*zeta*lambda  ;
+    -nu*U_tilde - epsilon*zeta*lambda          ,  zeta_alpha+epsilon*zeta*lambda^2     , epsilon*zeta*lambda  ;
     -zeta                                  ,  zeta*lambda                         , zeta                 ];
 
-K = [Omega^2 + epsilon*gamma      ,   beta*U_tilde^2-eta*gamma*lambda                 ,  -epsilon*gamma       ;
+K = [Omega^2 + epsilon*gamma      ,   beta*U_tilde^2-epsilon*gamma*lambda                 ,  -epsilon*gamma       ;
     -epsilon*gamma*lambda         , r_alpha^2-nu*U_tilde^2 + epsilon*gamma*lambda^2     ,  epsilon*gamma*lambda;
     -gamma                       , gamma*lambda                                       , gamma               ];
 
@@ -35,3 +38,20 @@ Fq = [xi_h*y^3+epsilon*xi(y-x_tilde-lambda*alpha)^3                ;
     xi*(x_tilde+lambda*alpha-y)^3                                ];
 
 %% Routh stability analysis
+
+% We analyse the stability of the airfoil by itself. Therefore, we truncate
+% the matrices.
+
+MM = M(1:2,1:2);
+KK = K(1:2,1:2);
+CC = C(1:2,1:2);
+
+
+B = [eye(2) eye(2);
+    -inv(MM)*KK -inv(MM)*CC];
+
+syms S(s)
+S(s) = det(B-s*eye(size(B)))
+
+
+
